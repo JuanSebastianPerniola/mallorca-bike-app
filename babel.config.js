bad2ece376df4e08787d6ba13357f82c171abb8f
JuanSebastianPerniola <juanperniolarugby@gmail.com>
2025-07-30 08:58:57 +0200

    a

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,7 @@
-module.exports = function (api) {
+export default function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [['module:react-native-dotenv']],
   };
 };
-// This configuration file sets up Babel for a React Native project using Expo.
-// It includes the 'babel-preset-expo' preset for Expo compatibility and the 'react
